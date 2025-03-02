1782b60ca9ba860bd2d9e75ccb22fa5bd1d749a8
Heather M. <heatherkmorrison@gmail.com>
2025-03-02 15:14:13 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (52.69%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -3,11 +3,12 @@ class Solution:
         counter = 0
         k = len(nums)
 
+        #calculate length of final list
         while counter < len(nums):
             if nums[counter] == val:
                 k -= 1
             counter += 1
         val_count = len(nums) - k
-        nums[:] = [num for num in nums if num != val]
-        nums.extend([None] * val_count)  
+        nums[:] = [num for num in nums if num != val] #remove val from list
+        nums.extend([None] * val_count) #add None as filler at the end
         return k
