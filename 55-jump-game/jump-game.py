aec91860ef87b2481965e61dfb1e87a70080504f
Heather M. <heatherkmorrison@gmail.com>
2025-03-07 21:54:46 -0800

    Time: 36 ms (38.65%) | Memory: 18.5 MB (89.48%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,12 +1,12 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        count = 0
-        max = 0
+        count = 0 #spot we're checking
+        max = 0 #number of moves
         while count < len(nums):
             if nums[count] > max:
                 max = nums[count]
             if count < len(nums) - 1 and nums[count] == 0 and max == 0:
-                return False
+                return False #we have to be out of moves
             count += 1
             max -= 1
         return True
