ca0e397838b4a2e6568b7682a69751a7884144ff
Heather M. <heatherkmorrison@gmail.com>
2025-03-09 05:57:48 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (48.58%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,9 +1,9 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         count = 1
-        prefix = strs[0]
+        prefix = strs[0] #grab first string
         while count < len(strs):
-            while strs[count].startswith(prefix) == False and len(prefix) != 0:
+            while strs[count].startswith(prefix) == False and len(prefix) != 0: #increment backwards until we have a match or nothing
                 prefix = prefix[:-1]
             count += 1
         return prefix
