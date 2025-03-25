f9f82e7300755acd4e81d4b370e11448e4fefb29
Heather M. <heatherkmorrison@gmail.com>
2025-03-25 09:43:41 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (31.10%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -2,10 +2,10 @@ class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         number = ""
         count = 0
-        while count < len(digits):
+        while count < len(digits):#get rid of list
             number = number + str(digits[count])
             count += 1
-        total = int(number) + 1
-        converted = [int(char) for char in str(total)]
+        total = int(number) + 1 #increment
+        converted = [int(char) for char in str(total)]#convert back
         return converted
         
